67affffb6ea05a6ecb43552726cd7c57c462ad14
nkrowicki <nahuelkrowicki@gmail.com>
2018-07-17 20:18:20 -0300

    Fix js Desafio 15

--- Clase 15/Alumno/Desafío/index.js
+++ Clase 15/Alumno/Desafío/index.js
@@ -1,50 +1,53 @@
-let showNames = (results) => {
-  let names
-  for (let i = 0; i < results.length; i++) {
-    names = results[i]
+$(function () {
 
-    console.log(names.name)
+
+  let showNames = (results) => {
+    let names
+    for (let i = 0; i < results.length; i++) {
+      names = results[i]
+
+      console.log(names.name)
+    }
   }
-}
 
-let getData = (url, cbk) => {
+  let getData = (url, cbk) => {
 
-  let request = $.ajax(url)
+    let request = $.ajax(url)
 
-  request
-    .done(function (data) {
+    request
+      .done(function (data) {
 
-      cbk(null, data)
+        cbk(null, data)
 
-    })
-    .fail(function (error) {
+      })
+      .fail(function (error) {
 
-      cbk(error, null)
+        cbk(error, null)
 
 
-    })
-}
-
-let getInfo = (error, data) => {
-  if (!error) {
-    showNames(data.results)
-    if (data.next) getData(data.next, getInfo)
-    // console.log('Todo ok', data)
-  } else {
-    console.log('Error: ' + error)
+      })
   }
-}
 
-let urlBase = 'https://swapi.co/api/' + 'people/'
+  let getInfo = (error, data) => {
+    if (!error) {
+      showNames(data.results)
+      if (data.next) getData(data.next, getInfo)
+      // console.log('Todo ok', data)
+    } else {
+      console.log('Error: ' + error)
+    }
+  }
 
-getData(urlBase, getInfo)
+  let urlBase = 'https://swapi.co/api/' + 'people/'
+
+  getData(urlBase, getInfo)
 
 
 
 
 
 
-
+});
 
 
 
